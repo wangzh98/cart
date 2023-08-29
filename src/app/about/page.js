5e9ca3c0bec35aba3628ac74525626abaf120503
wangzh98 <78910421@qq.com>
2023-08-30 07:24:04 +0800

    add pages

---
+++ src/app/about/page.js
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export default function Page() {
+    return (
+        <>
+            <h1>about</h1>
+            <form>
+                 <input/>
+                 <Link href="..">Cancel</Link>
+            </form>
+        </>
+    )
+}
---
+++ src/app/list/page.js
@@ -0,0 +1,5 @@
+export default function List() {
+    return (
+        <h1>List</h1>
+    )
+}
