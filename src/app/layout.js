7ff5f9543863a0e23b567d2495a9cde7189172bc
wangzh98 <78910421@qq.com>
2023-08-30 07:56:17 +0800

    add components

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-slate-800 text-slate-100 container mx-auto p-4`}>
+      <body className={`${inter.className}`}>
         {children}
       </body>
     </html>
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link"
+import Header from "../componets/header";
 
 export default function Home() {
     return (
         <>
-            <header>
-                <h1 className={"text-2xl"}>TODOs</h1>
-                <Link href="/about">New</Link>
-            </header>
+        <Header></Header>
+            {/*<header>*/}
+            {/*    <h1 className={"text-2xl"}>TODOs</h1>*/}
+            {/*    <Link href="/about">New</Link>*/}
+            {/*</header>*/}
         </>
     )
 }
